6a6bcdb7eeb92faddebf3a17434ed0dc4ed3594f
Vignesh253 <142732211+Vignesh253@users.noreply.github.com>
2024-10-17 19:34:28 +0530

    Add files via upload
    a simple face detection using py

---
+++ facedetective.py
@@ -0,0 +1,16 @@
+import cv2
+a = cv2.CascadeClassifier("casforndef.xml")
+b = cv2.VideoCapture(0)
+while True:
+    c_rec,d_img= b.read()
+    e = cv2.cvtColor(d_img,cv2.COLOR_BGR2GRAY)
+    f = a.detectMultiScale(e,1.3,6)
+    
+    for(x1,y1,w1,h1) in f:
+        cv2.rectangle(d_img,(x1,y1),(x1+w1,y1+h1),(0,255,0),10)
+    cv2.imshow('img',d_img)
+    h = cv2.waitKey(10)& 0xff
+    if h == 10:
+        break
+b.release()
+cv2.destroyAllWindows()    
